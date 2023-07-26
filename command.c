c12889f1b308e4d3076f96e67785fb3cce0c097d
hajarzarrouki <hajarzarrouki3@gmail.com>
2023-07-26 13:47:52 -0700

    command.c

--- command.c
+++ command.c
@@ -2,24 +2,45 @@
 
 char *_get_command(char *command)
 {
-    char *path = _get_env("PATH");
-    char *token;
-    char *cmd_full;
-    struct stat st;
+char *path = _get_env("PATH");
+char *token;
+char *cmd_full;
+struct stat st;
 
-    token = strtok(path, ":");
-    while (token)
-    {
-        cmd_full = malloc(_strlen(token) + _strlen(command) + 2);
-        strcpy(cmd_full, token);
-        strcat(cmd_full, "/");
-        strcat(cmd_full, command);
-        if (stat(cmd_full, &st) == 0)
-        {
-            return (cmd_full);
-        }
-        free(cmd_full);
-        token = strtok(NULL, ":");
-    }
-    return (NULL);
+token = strtok(path, ":");
+while (token)
+{
+	cmd_full = malloc(_strlen(token) + _strlen(command) + 2);
+strcpy(cmd_full, token);
+strcat(cmd_full, "/");
+strcat(cmd_full, command);
+if (stat(cmd_full, &st) == 0)
+{
+return (cmd_full);
+}
+free(cmd_full);
+token = strtok(NULL, ":");
+}
+
+void ls - r(char *path)
+{
+	char *cmd;
+	list_cmd = 0;
+	struct path
+	{
+		int chdir(const char *path);
+		struct path *next;
+		struct token *path = NULL;
+		if (path == NULL)
+		printf("list is empty\n")
+			return;
+		printf("path of signaly linked lis\n")
+			
+			int data;
+			while(current != NULL)
+			printf("%d", current->data);
+			current = current->next;
+			printf("\n");
+			return 0;
+	}
 }
